4c3c7386aaa52cbc29ed47292c00529ffda08704
Riddhi Deshmukh <riddhi.dd2008@gmail.com>
2026-09-24 19:49:41 +0530

    Added updated version

--- Study_TIme_Calculator.py
+++ Study_TIme_Calculator.py
@@ -1,12 +1,22 @@
 print("===== STUDY TIME CALCULATOR =====")
-
+daily_goal=float(input("Enter your daily study goal:"))
 subjects = int(input("Enter number of subjects: "))
-
 total_time = 0
 
 for i in range(subjects):
-    time = float(input(f"Enter study time for subject {i + 1} (hours): "))
-    total_time += time
+ name=input(f"\nEnter subject {i+1}name:")
+ time=float(input("Enter study time for {name}(hours):"))
+ total_time+=time
+print("\n====RESULT====")
+print("Total study time:",total_time,"hours")
+print('average study time:',round(total_time/subjects,2),"hours")
+remaining = daily_goal-total_time
+if remaining>0:
+  print("Study time remaining,""hours")
+elif remaining==0:
+  print("Great!you completed your daily goal!")
+else: 
+  print("You ecceeded your daily goal by:",abs(remaining),"hours")
 
 print("\nTotal study time:", total_time, "hours")
 print("Average study time:", total_time / subjects, "hours")
